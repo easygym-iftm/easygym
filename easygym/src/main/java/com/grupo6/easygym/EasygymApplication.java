17adcba0837140d6a74e509c7b47431d16893b44
alexm <alexm@LAPTOP-GH25Q7KA>
2020-10-14 20:19:00 -0300

    Projeto Grupo 06 - EasyGym

---
+++ easygym/src/main/java/com/grupo6/easygym/EasygymApplication.java
@@ -0,0 +1,13 @@
+package com.grupo6.easygym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasygymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasygymApplication.class, args);
+	}
+
+}
---
+++ easygym/src/test/java/com/grupo6/easygym/EasygymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo6.easygym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasygymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
